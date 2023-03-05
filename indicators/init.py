ffdc6b727e64b66f219babf10ac77592c24ac187
Anand Damdiyal <126330418+AnandDamdiyal@users.noreply.github.com>
2023-03-05 03:49:32 -0800

    Create init.py

---
+++ indicators/init.py
@@ -0,0 +1,16 @@
+# Import all indicators here
+from indicators.adx import ADX
+from indicators.aroon import Aroon
+from indicators.atr import ATR
+from indicators.bbands import BBands
+from indicators.cci import CCI
+from indicators.ema import EMA
+from indicators.ichimoku import Ichimoku
+from indicators.macd import MACD
+from indicators.obv import OBV
+from indicators.rsi import RSI
+from indicators.sma import SMA
+from indicators.stoch import Stoch
+from indicators.vwap import VWAP
+from indicators.willr import WillR
+from indicators.zigzag import ZigZag
