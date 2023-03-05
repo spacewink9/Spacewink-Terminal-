c98080a2075697f0e563a6782ab85a3fd01c0ad3
Anand Damdiyal <126330418+AnandDamdiyal@users.noreply.github.com>
2023-03-05 05:58:47 -0800

    Create price_to_earnings_ratio.py

---
+++ fundamental_analysis/advanced_fundamental_analysis/price_to_earnings_ratio.py
@@ -0,0 +1,13 @@
+def calculate_price_to_earnings_ratio(price_per_share, earnings_per_share):
+    """
+    Calculates the price-to-earnings (P/E) ratio given the price per share and earnings per share.
+
+    Args:
+        price_per_share (float): The price per share of the stock.
+        earnings_per_share (float): The earnings per share of the stock.
+
+    Returns:
+        float: The P/E ratio of the stock.
+    """
+    return price_per_share / earnings_per_share
+   
