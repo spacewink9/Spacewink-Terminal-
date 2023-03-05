6408f611abe742b4c990faa4edd8c20d3a53140e
Anand Damdiyal <126330418+AnandDamdiyal@users.noreply.github.com>
2023-03-05 08:16:37 -0800

    Create voice_text.py

---
+++ utils/voice_text.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        return text
+    except:
+        return None
