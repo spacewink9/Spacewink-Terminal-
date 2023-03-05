952eda47131a3e6c3c8f87f5b53c424774218c95
Anand Damdiyal <126330418+AnandDamdiyal@users.noreply.github.com>
2023-03-05 07:03:54 -0800

    Update earnings_history.py

--- fundamental_analysis/earning/earnings_history.py
+++ fundamental_analysis/earning/earnings_history.py
@@ -1,11 +1,14 @@
-import yfinance as yf
+import requests
+import pandas as pd
 
-def get_earnings_history(ticker):
-    # Get the ticker data
-    ticker_data = yf.Ticker(ticker)
-
-    # Get the earnings history
-    earnings_history = ticker_data.earnings
-
-    # Return the earnings history
-    return earnings_history
+class EarningsHistory:
+    def __init__(self, symbol):
+        self.symbol = symbol
+    
+    def get_earnings_history(self):
+        url = f"https://www.alphavantage.co/query?function=EARNINGS&symbol={self.symbol}&apikey=<YOUR_API_KEY>"
+        res = requests.get(url)
+        data = res.json()
+        earnings_data = data["quarterlyEarnings"]
+        earnings_df = pd.DataFrame(earnings_data)
+        return earnings_df
