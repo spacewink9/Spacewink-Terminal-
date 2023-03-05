ee61373afb570580463dd35e651dca11b7c5d5ef
Anand Damdiyal <126330418+AnandDamdiyal@users.noreply.github.com>
2023-03-05 06:03:46 -0800

    Create debt_to_equity_ratio.py

---
+++ fundamental_analysis/advanced_fundamental_analysis/debt_to_equity_ratio.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def calculate_debt_to_equity_ratio(financial_data):
+    """
+    Calculates the debt-to-equity ratio for a given financial dataset.
+
+    Args:
+    - financial_data (pandas.DataFrame): The financial data to analyze. This should contain columns for total debt
+                                           and shareholder equity.
+
+    Returns:
+    - float: The debt-to-equity ratio.
+    """
+    total_debt = financial_data['total_debt'].iloc[-1]
+    shareholder_equity = financial_data['shareholder_equity'].iloc[-1]
+
+    debt_to_equity_ratio = total_debt / shareholder_equity
+
+    return debt_to_equity_ratio
+
+def analyze_company_debt_to_equity_ratio(data):
+    """
+    Analyzes the debt-to-equity ratio for a given company.
+
+    Args:
+    - data (dict): A dictionary containing the financial data for the company.
+
+    Returns:
+    - str: A message summarizing the company's debt-to-equity ratio and whether it is favorable or unfavorable.
+    """
+    financial_data = pd.DataFrame(data)
+
+    debt_to_equity_ratio = calculate_debt_to_equity_ratio(financial_data)
+
+    if debt_to_equity_ratio < 1:
+        message = "The company has a favorable debt-to-equity ratio of {0:.2f}.".format(debt_to_equity_ratio)
+    else:
+        message = "The company has an unfavorable debt-to-equity ratio of {0:.2f}.".format(debt_to_equity_ratio)
+
+    return message
